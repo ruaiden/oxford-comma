9a779726c3f639729af085305bd38345d3105809
Aiden <aiden.marques94@gmail.com>
2020-11-14 16:58:27 -0500

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
-def oxford_comma(array)
 
+def oxford_comma(array)
+    if array.length == 1
+    arr = array.join("") {puts arr}
+    elsif array.length == 2
+    arra = array.join(" and ") {puts arra}
+    else 
+    array[-1].insert(0,"and ")
+    array.join(", ")
+    end 
 end
+
